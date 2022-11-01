8343d483733a8baf4031c4b9aaae4cd2a22f5187
YoungJoon <93526791+youngjoon568@users.noreply.github.com>
2022-11-01 20:55:31 +0900

    최종본
    변수 이름 수정 관련 오류 수정

--- RockScissorsPaper/RockScissorsPaper.js
+++ RockScissorsPaper/RockScissorsPaper.js
@@ -6,19 +6,18 @@ const $text = $(".body");
 const $userTargets = [...document.querySelectorAll(".user img")];
 
 $target.id = "rock";
-let count1 = 1;
-let count2 = 1;
+let count = 1;
 let timer = null;
 let eventTarget = null;
 
 const changeImage = () => {
-    if (count1 == 3) {
-        count1 = 0;
+    if (count == 3) {
+        count = 0;
     }
 
-    count1++;
-    $target.id = count1;
-    $target.src = `./image/${count1}.png`;
+    count++;
+    $target.id = count == 1 ? "rock" :  count == 2 ? "scissors" : count == 3 ? "paper" : "";
+    $target.src = `./image/${count}.png`;
 };
 
 const timerHandle = () => {
@@ -43,10 +42,9 @@ const startGame = () => {
 };
 
 const resultGame = (user, bot) => {
-    console.log(user, bot);
     if ((user == "rock" && bot == "scissors") || (user == "scissors" && bot == "paper") || (user == "paper" && bot == "rock")) {
         $text.innerText = "승리";
-    } else if ((user == "rock" && bot == "rock") || (user == "scissors" && bot == "scissors") || (user == "paper" && bot == "paper")) {
+    } else if ((user == bot) || (user == "scissors" && bot == "scissors") || (user == "paper" && bot == "paper")) {
         $text.innerText = "무승부";
     } else {
         $text.innerText = "패배";
