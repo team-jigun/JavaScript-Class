c146f56254af91156c662b86d7ec1a55c2dce44a
Happi3r <hsphs0101@gmail.com>
2022-11-02 09:44:10 +0900

    변수 수정
    Clock - 변수 가독성 개선
    first - 파라미터 문법 개선

--- Clock/Clock.js
+++ Clock/Clock.js
@@ -1,7 +1,7 @@
 setInterval(() => {
     const now = new Date();
-    const h = String(now.getHours()).padStart(2, '0');
-    const m = String(now.getMinutes()).padStart(2, '0');
-    const s = String(now.getSeconds()).padStart(2, '0');
-    document.querySelector('.title').innerHTML = `${h}:${m}:${s}`;
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const secs = String(now.getSeconds()).padStart(2, '0');
+    document.querySelector('.title').innerHTML = `${hours}:${minutes}:${secs}`;
 }, 1000);
--- Task/first.js
+++ Task/first.js
@@ -9,10 +9,10 @@ Ex)
 
 // 2-1. message는 매개 변수를 받은 만큼 *로 감싼 문자가 출력되어야 한다.
 // 2-2. 매개 변수는 배열로 받지 않는다.
-function printer() {
-  const max = Math.max(...[...arguments].map(v => String(v).length));
+function printer(...args) {
+  const max = Math.max(...[...args].map(v => String(v).length));
   console.log('*'.repeat(max + 4));
-  for (let v of arguments) {
+  for (let v of args) {
     const space = max - String(v).length;
     console.log(`* ${' '.repeat((space + 1) / 2)}${v}${' '.repeat(space / 2)} *`);
   }
