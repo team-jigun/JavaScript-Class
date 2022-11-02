e0f997d19c644e4c4923fa38c7ee6865e3f9553b
kwonjun <116526991+jun060703@users.noreply.github.com>
2022-11-02 19:59:58 +0900

    Update Clock.js

--- Clock/Clock.js
+++ Clock/Clock.js
@@ -3,10 +3,10 @@ clockTitle = clockContainer.querySelector("h1");
 
 function getTime() {
     const date = new Date();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
-    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+    const hours = String(date.gethours()).padStart(2, "0");
+    const min = String(date.getminutes()).padStart(2, "0");
+    const sec = String(date.getseconds()).padStart(2, "0");
+    clockTitle.innerText = `${hours}:${min}:${sec}`;
 } 
 function init() {
     getTime();
