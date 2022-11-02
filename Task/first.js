0711d7ccad4d5087418266210e3f84fefe27a6c2
OhSangHyuk <91905776+OhSangHyuk@users.noreply.github.com>
2022-11-02 19:58:27 +0900

    오상혁-숙제제출

--- Task/first.js
+++ Task/first.js
@@ -7,12 +7,20 @@ Ex)
 *********
 */
 
-// 2-1. message는 매개 변수를 받은 만큼 *로 감싼 문자가 출력되어야 한다.
-// 2-2. 매개 변수는 배열로 받지 않는다.
-function printer(message) {
-  console.log('**********');
-  console.log('*' + message + '*');
-  console.log('**********');
+function StarPrint(message) {
+  star = "**";
+  for (let i = 0; i < message.length; i++) {
+    star += "*";
+  }
+  console.log(star);
+  console.log("*" + message + "*");
+  console.log(star);
 }
 
-printer('test');
+// 2-1. message는 매개 변수를 받은 만큼 *로 감싼 문자가 출력되어야 한다.
+// 2-2. 매개 변수는 배열로 받지 않는다.
+function printer() {
+  for (let i of arguments) {
+    StarPrint(i);
+  }
+}
