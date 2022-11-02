8b82f4fd4e1e8df71e441a91b304f80578a32f70
mingi502 <116711211+mingi502@users.noreply.github.com>
2022-11-02 19:43:06 +0900

    Update Clock.js

--- Clock/Clock.js
+++ Clock/Clock.js
@@ -1,12 +1,11 @@
 const clock = document.querySelector('.title');
 
-
 function getTime(){
     const time = new Date();
     const hour = time.getHours();
-    const minutes = time.getMinutes();
-    const seconds = time.getSeconds();
-    clock.innerHTML = `${hour}:${minutes}:${seconds}`
+    const minute = time.getMinutes();
+    const second = time.getSeconds();
+    clock.innerHTML = `${hour}:${minute}:${second}`
 }
 
 function set(){
