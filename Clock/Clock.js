8934a6df068f47b24f00f9bb8f8092f4646eb4a0
jaewon lee <jwleetop5@naver.com>
2022-11-03 21:59:10 +0900

    update

--- Clock/Clock.js
+++ Clock/Clock.js
@@ -1,16 +1,13 @@
-let day = new Date();
 const title = document.querySelector("div > h1.title");
-let h = String(day.getHours()).padStart(2, "0");
-let m = String(day.getMinutes()).padStart(2, "0");
-let s = String(day.getSeconds()).padStart(2, "0");
-
-title.textContent = `${h}:${m}:${s}`;
-
-setInterval(() => {
-  day = new Date();
-  h = String(day.getHours()).padStart(2, "0");
-  m = String(day.getMinutes()).padStart(2, "0");
-  s = String(day.getSeconds()).padStart(2, "0");
+function resetTimeAndChangeTime() {
+  let day = new Date();
+  let h = String(day.getHours()).padStart(2, "0");
+  let m = String(day.getMinutes()).padStart(2, "0");
+  let s = String(day.getSeconds()).padStart(2, "0");
 
   title.textContent = `${h}:${m}:${s}`;
+}
+resetTimeAndChangeTime();
+setInterval(() => {
+  resetTimeAndChangeTime();
 }, 1000);
