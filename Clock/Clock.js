74f8ff7d3cd3b4e5fe6530491dd373091dd7573b
우현민 <85679132+hammi1015@users.noreply.github.com>
2022-11-03 22:40:53 +0900

    우현민 과제제출 3번째
    3번째 재제출입니다.

--- Clock/Clock.js
+++ Clock/Clock.js
@@ -2,7 +2,12 @@ let clock = document.querySelector(".title");
 
 function time() {
   let date = new Date();
-
-  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+  let second = date.getSeconds();
+  clock.innerText = `${hour < 10 ? `0${hour}` : hour}:${
+    minute < 10 ? `0${minute}` : minute
+  }:${second < 10 ? `0${second}` : second}`;
 }
+
 setInterval(time, 1000);
--- Task/first.js
+++ Task/first.js
@@ -4,7 +4,7 @@
 
 function printer1(message) {
   let star = "**";
-  for (let i = 0; i < message.length; i++) {
+  for (let i = 0; i < String(message).length; i++) {
     star += "*";
   }
   console.log(star);
@@ -22,8 +22,8 @@ Ex)
 // 2-1. message는 매개 변수를 받은 만큼 *로 감싼 문자가 출력되어야 한다.
 
 // 2-2. 매개 변수는 배열로 받지 않는다.
-function printer() {
-  for (let i of arguments) {
+function printer(...pararm) {
+  for (let i of pararm) {
     printer1(i);
   }
 }
