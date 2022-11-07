b7fd85da22635aefd1a0d82f58d22c8d15942e93
jammykii <85679149+jammykii@users.noreply.github.com>
2022-11-07 13:20:59 +0900

    Revert "김준철"

--- Task/first.js
+++ Task/first.js
@@ -6,35 +6,13 @@ Ex)
 * test2 *
 *********
 */
-function printer1(test2) {
-  let result = max(arguments) + 2;
-  console.log("*".repeat(result))
-    console.log("*" + test2 + "*")
-  console.log("*".repeat(result))
-}
-
 
 // 2-1. message는 매개 변수를 받은 만큼 *로 감싼 문자가 출력되어야 한다.
 // 2-2. 매개 변수는 배열로 받지 않는다.
-function printer2(...parameter) {
-  let result = max(parameter) + 2;
-  console.log('*'.repeat(result + 2));
-  for (let i of parameter) {
-    console.log(
-      '*' +
-      ' '.repeat((result - String(i).length + 1) / 2) +
-      i +
-      ' '.repeat((result - String(i).length) / 2) +
-      '*'
-    );
-  }
-  console.log('*'.repeat(result + 2));  
+function printer(message) {
+  console.log('**********');
+  console.log('*' + message + '*');
+  console.log('**********');
 }
 
-function max(args) {
-  let result = -2147483648;
-  for (let i of args) {
-    if (result < String(i).length) result = String(i).length;
-  }
-  return result;
-}
+printer('test');
