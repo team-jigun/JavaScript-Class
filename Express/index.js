7fcc4e08645d7f13caa22066bdaf979308daca34
mingi502 <116711211+mingi502@users.noreply.github.com>
2022-11-07 20:48:02 +0900

    Create index.js

---
+++ Express/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.get('/plus', (req, res)=>{
+
+    const plus = Number(req.query.a) + Number(req.query.b)
+
+    res.send(`${plus}`);
+});
+
+app.get('/minus', (req, res)=>{
+
+    const minus = Number(req.query.a) - Number(req.query.b)
+
+    res.send(`${minus}`);
+})
+
+app.listen(port, ()=>{
+    console.log(`Server On! ${port}`)
+});
