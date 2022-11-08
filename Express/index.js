331719a6b079ef46f6a7ecc50bd8ced6e2bec454
이상민 <sdh210415@sdh.hs.kr>
2022-11-08 23:40:47 +0900

    5일차과제 수정

--- Express/index.js
+++ Express/index.js
@@ -1,25 +1,16 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;// 앞의 값이 null일 경우 뒤에 값을 출력해주세요?
+const port = process.env.PORT || 3000;
 
 app.get('/plus', (req,res) => {
-  console.log('Post test');
-  
-  
-
-
   let plusNum1 = parseInt(req.query.plusNum1);
-  let plusNum2 = parseInt(req.query.plusNum2);
-  
+  let plusNum2 = parseInt(req.query.plusNum2);  
   res.send(`${plusNum1+plusNum2}`);
 });
 
 app.get('/minus', (req,res) => {
-  console.log('Post test');
-  
   let minusNum1 = parseInt(req.query.minusNum1);
   let minusNum2 = parseInt(req.query.minusNum2);
-  
   res.send(`${minusNum1-minusNum2}`);
 });
 
