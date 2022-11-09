3daccdb4ed68cb1e55b19c264e9911f3a8036b91
OhSangHyuk <91905776+OhSangHyuk@users.noreply.github.com>
2022-11-09 19:10:51 +0900

    오상혁-Express과제 제출

--- Express/index.js
+++ Express/index.js
@@ -5,19 +5,19 @@ const port = process.env.PORT || 3000;
 app.get("/plus", (req, res) => {
   console.log("Post test");
 
-  let PlusNum1 = parseInt(req.query.plusNum1);
-  let PlusNum2 = parseInt(req.query.plusNum2);
+  let PNum1 = parseInt(req.query.plusNum1);
+  let PNum2 = parseInt(req.query.plusNum2);
 
-  res.send(`${PlusNum1 + PlusNum2}`);
+  res.send(`${PNum1 + PNum2}`);
 });
 
 app.get("/minus", (req, res) => {
   console.log("Post test");
 
-  let minusNum1 = parseInt(req.query.minusNum1);
-  let minusNum2 = parseInt(req.query.minusNum2);
+  let MNum1 = parseInt(req.query.minusNum1);
+  let MNum2 = parseInt(req.query.minusNum2);
 
-  res.send(`${minusNum1 - minusNum2}`);
+  res.send(`${MNum1 - MNum2}`);
 });
 
 app.listen(port, (_) => {
