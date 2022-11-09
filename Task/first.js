211f1519c4d9a1018f2d68f8551ee2db8c8de807
jooyoung050210 <85738818+sdh210411@users.noreply.github.com>
2022-11-09 18:28:11 +0900

    4일차 과제
    늦게 올려서 죄송합니다ㅠ

--- Task/first.js
+++ Task/first.js
@@ -6,17 +6,22 @@ Ex)
 * test2 *
 *********
 */
+function whgdk (message){
+ let star = "**";
+  for(let i = 0; i<String(message).length; i++){
+    star += "*"
+
+  }
+  console.log(star);
+  console.log("*" + message + "*");
+  console.log(star);
+
+} 
 
 // 2-1. message는 매개 변수를 받은 만큼 *로 감싼 문자가 출력되어야 한다.
 // 2-2. 매개 변수는 배열로 받지 않는다.
-function printer(...args) {
-  const max = Math.max(...[...args].map(v => String(v).length));
-  console.log('*'.repeat(max + 4));
-  for (let v of args) {
-    const space = max - String(v).length;
-    console.log(`* ${' '.repeat((space + 1) / 2)}${v}${' '.repeat(space / 2)} *`);
+function rnt (...para){
+  for(let i of para){
+    whgdk(i);
   }
-  console.log('*'.repeat(max + 4));
 }
-
-printer('test2', 199999, 1271799);
